fe8855dc13c9d6c410ca9182f4d58d6a16bb516a
houssam-mahlous <houssam.mahlous@brandwatch.com>
2021-08-16 11:04:14 +0100

    add common dependency to api  module

--- api/src/main/java/com/brandwatch/minibcr/api/ApiApplication.java
+++ api/src/main/java/com/brandwatch/minibcr/api/ApiApplication.java
@@ -3,7 +3,7 @@ package com.brandwatch.minibcr.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.brandwatch.minibcr.api")
+@SpringBootApplication(scanBasePackages = {"com.brandwatch.minibcr.api"})
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class,args);
