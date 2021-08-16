9dd88027c6ae85b4f321f7336c1f73286560ef13
houssam-mahlous <houssam.mahlous@brandwatch.com>
2021-08-16 11:25:46 +0100

    Add failsafe plugin to root pom.xml
    Fix lint issues

--- api/src/main/java/com/brandwatch/minibcr/api/ApiApplication.java
+++ api/src/main/java/com/brandwatch/minibcr/api/ApiApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.brandwatch.minibcr.api"})
 public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class,args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
